a7ebbe11d9da86f678f85fa0464943c221f016a3
Warren Galyen <wgalyen@gmail.com>
2020-03-09 10:56:59 -0700

    Fixed build issue on Mac OSX

--- installers/osx-tar-gz/package.php
+++ installers/osx-tar-gz/package.php
@@ -14,6 +14,7 @@ $rootpath = str_replace("\\", "/", dirname(__FILE__));
 // Determine the OS.
 $os      = php_uname("s");
 $windows = (strtoupper(substr($os, 0, 3)) == "WIN");
+$mac = (strtoupper(substr($os, 0, 6)) == "DARWIN");
 
 if ($windows) {
 	echo "This script might not work as expected on Windows. Running this script via a reasonable Bash port (e.g. git bash) is recommended for tar/gzip support.\n\n";
@@ -163,7 +164,7 @@ file_put_contents($stagingpath . "/Contents/Info.plist", $data);
 echo "Generating " . $appname . "-" . $packageinfo["app_ver"] . "-osx.tar.gz...\n";
 chdir($rootpath);
 @unlink($appname . "-" . $packageinfo["app_ver"] . "-osx.tar.gz");
-system("tar czvf " . escapeshellarg($appname . "-" . $packageinfo["app_ver"] . "-osx.tar.gz") . " --owner=0 --group=0 " . escapeshellarg($packageinfo["app_name"] . " " . $packageinfo["app_ver"] . " Installer.app/"));
+system("tar czvf " . escapeshellarg($appname . "-" . $packageinfo["app_ver"] . "-osx.tar.gz") . ($mac ? " " : " --owner=0 --group=0 ") . escapeshellarg($packageinfo["app_name"] . " " . $packageinfo["app_ver"] . " Installer.app/"));
 
 echo "Cleaning up...\n";
 DirHelper::Delete($stagingpath);
